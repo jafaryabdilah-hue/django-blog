2d5da1a4e172fa0704a9fb5bb9bc5de49ce1cefe
Idrisjafary <you@example.com>
2025-12-28 21:49:09 +0700

    update blog

--- mysite/settings.py
+++ mysite/settings.py
@@ -118,7 +118,13 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = str(BASE_DIR / 'staticfiles')
+
+STATICFILES_DIRS = [
+    BASE_DIR / 'blog/static',
+]
+
+STATIC_ROOT = BASE_DIR / 'staticfiles'
+
 
 
 # Default primary key field type
